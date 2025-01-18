1f2c93c923b355101d3fca2cb7dbd324e3767427
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-01-18 20:07:25 -0300

    (style): CSS da Home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -24,7 +24,16 @@ const Home = () => {
   return (
 
     <div className="home">
-      Home
+      <h2>Confira as últimas Memórias</h2>
+      <div className="memories-container">
+        {memories.length > 0 && memories.map((memory) => 
+          <div className="memory" key={memory._id}>
+            <img src={`${axios.defaults.baseURL}${memory.src}`} alt={memory.title} />
+            <p>{memory.title}</p>
+            <Link className="btn" to={`/memories/${memory._id}`}>Comentar</Link>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
