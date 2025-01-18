bd744ec7345ac85a45f668f9a176d8a784bba666
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-01-18 19:06:41 -0300

    (wip): Enviando dados form

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,25 +1,59 @@
-import axios from "axios"
+import axios from "../axios-config"
 
 import { useState } from "react"
 
 import './AddMemory.css'
 
 const AddMemory = () => {
+
+  const [inputs, setInputs] = useState({})
+  const [image, setImage] = useState(null)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    console.log(inputs, image)
+
+    const formData = new FormData()
+    formData.append('title', inputs.title)
+    formData.append('description', inputs.description)
+    formData.append('image', image)
+
+    try {
+      const response = await axios.post('/memories', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+    } catch (error) {
+      console.error(error)
+      
+    }
+  }
+
+  const handleChange = (event) => {
+    if (event.target.name === 'image') {
+      setImage(event.target.files[0])
+    } else {
+      setInputs({...inputs, [event.target.name]: event.target.value})
+    }
+  }
+
   return (
     <div className="add-memory-page">
       <h2>Crie uma nova memória</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Título:</p>
-          <input type="text" placeholder="Defina um título" name="title"/>
+          <input type="text" placeholder="Defina um título" name="title" onChange={handleChange}/>
         </label>
         <label>
           <p>Descrição:</p>
-          <textarea placeholder="Explique o que aconteceu..." name="description"></textarea>
+          <textarea placeholder="Explique o que aconteceu..." name="description" onChange={handleChange}></textarea>
         </label>
         <label>
           <p>Foto:</p>
-          <input type="file" name="image"/>
+          <input type="file" name="image" onChange={handleChange}/>
         </label>
         <input type="submit" className="btn" value="Enviar" />
       </form>
