8aab512102495a1b23261f8acb351edeabfe1fbe
Spinelli666 <bernardospinelli@hotmail.com.br>
2025-01-18 15:19:49 -0300

    (wip): Inserindo dados no sistema

--- backend/controllers/MemoryController.js
+++ backend/controllers/MemoryController.js
@@ -1,7 +1,28 @@
 const Memory = require('../models/Memory')
 
 const createMemory = async(req, res) => {
-    res.json("Deu certo")
+    try {
+ 
+        const { title, description } = req.body
+
+        const src = `images/${req.file.filename}`
+
+        if(!title || !description) {
+            return res.status(400).json({ msg: 'Title and description are required' })
+        }
+
+        const newMemory = new Memory({
+            title, src, description
+        })
+
+        await newMemory.save()
+
+        res.json({ msg: 'Memory created', newMemory})
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send('Ocorreu um erro')
+    }
 }
 
 module.exports = {
--- backend/routes.js
+++ backend/routes.js
@@ -2,8 +2,18 @@ const express = require('express')
 
 const router = express.Router()
 
+const upload = require('./helpers/upload')
+
 const { createMemory } = require('./controllers/MemoryController')
 
-router.post("/", (req, res) => createMemory(req, res))
+router.post("/", upload.single('image'), (req, res, next) => {
+    const image = req.file
+
+    if(!image) {
+        res.status(400).json({ message: 'Image is required' })
+    }
+
+    next()
+}, (req, res) => createMemory(req, res))
 
 module.exports = router
